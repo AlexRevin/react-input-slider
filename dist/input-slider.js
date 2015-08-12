395929f586656d34892e86664ef4a70c56869f96
Alex <alex@jaralex.ee>
2015-08-12 13:46:42 +0300

    Update input-slider.js

--- dist/input-slider.js
+++ dist/input-slider.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var cx = require('classnames');
 var React = require('react');
 
@@ -14,7 +18,7 @@ module.exports = React.createClass({
     ymin: React.PropTypes.number
   },
 
-  getDefaultProps() {
+  getDefaultProps: function getDefaultProps() {
     return {
       axis: 'x',
       xmin: 0,
@@ -22,53 +26,51 @@ module.exports = React.createClass({
     };
   },
 
-  render() {
+  render: function render() {
     var pos = this.getPosition();
     var axis = this.props.axis;
     var valueStyle = {};
-    if(axis === 'x') valueStyle.width = pos.left;
-    if(axis === 'y') valueStyle.height = pos.top;
+    if (axis === 'x') valueStyle.width = pos.left;
+    if (axis === 'y') valueStyle.height = pos.top;
 
-    return (
-      <div {...this.props}
-        className={cx('u-slider', `u-slider-${axis}`, this.props.className)}
-        onClick={this.handleClick}>
-        <div
-          className="value"
-          style={valueStyle}>
-        </div>
-        <div
-          className="handle"
-          ref="handle"
-          onMouseDown={this.handleMounseDown}
-          onClick={function(e) {
-            e.stopPropagation();
-            e.nativeEvent.stopImmediatePropagation();
-          }}
-          style={pos}>
-        </div>
-      </div>
+    return React.createElement(
+      'div',
+      _extends({}, this.props, {
+        className: cx('u-slider', 'u-slider-' + axis, this.props.className),
+        onClick: this.handleClick }),
+      React.createElement('div', {
+        className: 'value',
+        style: valueStyle }),
+      React.createElement('div', {
+        className: 'handle',
+        ref: 'handle',
+        onMouseDown: this.handleMounseDown,
+        onClick: function (e) {
+          e.stopPropagation();
+          e.nativeEvent.stopImmediatePropagation();
+        },
+        style: pos })
     );
   },
 
-  getPosition() {
-    var top = (this.props.y-this.props.ymin)/(this.props.ymax-this.props.ymin)*100;
-    var left = (this.props.x-this.props.xmin)/(this.props.xmax-this.props.xmin)*100;
+  getPosition: function getPosition() {
+    var top = (this.props.y - this.props.ymin) / (this.props.ymax - this.props.ymin) * 100;
+    var left = (this.props.x - this.props.xmin) / (this.props.xmax - this.props.xmin) * 100;
 
-    if(top > 100) top = 100;
-    if(top < 0) top = 0;
-    if(this.props.axis === 'x') top = 0;
+    if (top > 100) top = 100;
+    if (top < 0) top = 0;
+    if (this.props.axis === 'x') top = 0;
     top += '%';
 
-    if(left > 100) left = 100;
-    if(left < 0) left = 0;
-    if(this.props.axis === 'y') left = 0;
+    if (left > 100) left = 100;
+    if (left < 0) left = 0;
+    if (this.props.axis === 'y') left = 0;
     left += '%';
 
-    return {top: top, left: left};
+    return { top: top, left: left };
   },
-  
-  getCoords(pos) {
+
+  getCoords: function getCoords(pos) {
     var rect = this.getDOMNode().getBoundingClientRect();
     var width = rect.width;
     var height = rect.height;
@@ -76,23 +78,23 @@ module.exports = React.createClass({
     var top = pos.top;
     var axis = this.props.axis;
 
-    if(left < 0) left = 0;
-    if(left > width) left = width;
-    if(top < 0) top = 0;
-    if(top > height) top = height;
+    if (left < 0) left = 0;
+    if (left > width) left = width;
+    if (top < 0) top = 0;
+    if (top > height) top = height;
 
     var x = 0;
     var y = 0;
-    if(axis === 'x' || axis === 'xy') {
-      x = left/width*(this.props.xmax-this.props.xmin) + this.props.xmin;
+    if (axis === 'x' || axis === 'xy') {
+      x = left / width * (this.props.xmax - this.props.xmin) + this.props.xmin;
     }
-    if(axis === 'y' || axis === 'xy') {
-      y = top/height*(this.props.ymax-this.props.ymin) + this.props.ymin;
+    if (axis === 'y' || axis === 'xy') {
+      y = top / height * (this.props.ymax - this.props.ymin) + this.props.ymin;
     }
-    return {x: x, y: y}
+    return { x: x, y: y };
   },
 
-  handleMounseDown(e) {
+  handleMounseDown: function handleMounseDown(e) {
     var dom = this.refs.handle.getDOMNode();
 
     this.start = {
@@ -109,44 +111,43 @@ module.exports = React.createClass({
     document.addEventListener('mouseup', this.handleDragEnd);
   },
 
-  handleDrag(e) {
+  handleDrag: function handleDrag(e) {
     var rect = this.getDOMNode().getBoundingClientRect();
     var posX = e.clientX + this.start.x - this.offset.x;
     var posY = e.clientY + this.start.y - this.offset.y;
 
-    if(this.props.onChange) {
+    if (this.props.onChange) {
       var coords = this.getCoords({
         left: posX,
         top: posY
-      })
+      });
       this.props.onChange(coords);
     }
   },
 
-  handleDragEnd(e) {
+  handleDragEnd: function handleDragEnd(e) {
     document.removeEventListener('mousemove', this.handleDrag);
     document.removeEventListener('mouseup', this.handleDragEnd);
-    if(this.props.onDragEnd) {
+    if (this.props.onDragEnd) {
       var coords = this.getCoords({
-        left: (e.clientX - rect.left),
-        top: (e.clientY - rect.top)
-      })
+        left: e.clientX - rect.left,
+        top: e.clientY - rect.top
+      });
       this.props.onDragEnd(coords);
     }
   },
 
-  handleClick(e) {
+  handleClick: function handleClick(e) {
     var rect = this.getDOMNode().getBoundingClientRect();
     var coords = this.getCoords({
-        left: (e.clientX - rect.left),
-        top: e.clientY - rect.top
-      })
-    if(this.props.onChange) {
+      left: e.clientX - rect.left,
+      top: e.clientY - rect.top
+    });
+    if (this.props.onChange) {
       this.props.onChange(coords);
     }
-    if(this.props.onDragEnd) {
+    if (this.props.onDragEnd) {
       this.props.onDragEnd(coords);
     }
-
   }
 });
